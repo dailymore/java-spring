5ef219f30a641009317f0424211460ac24a647d4
Cong Minh <ndmc.system176@gmail.com>
2025-03-03 14:32:21 +0700

    fix: seed format

--- src/main/java/com/example/spring/utils/ArgonHash.java
+++ src/main/java/com/example/spring/utils/ArgonHash.java
@@ -7,7 +7,6 @@ public class ArgonHash {
 	private static final Argon2 argon2 = Argon2Factory.create();
 
 	public static String hash(char[] password) {
-
 		return argon2.hash(10, 65536, 1, password); // 10 vòng, 64MB RAM, 1 luồng CPU
 	}
 
