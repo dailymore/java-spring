a7f50f1e5c10283d555e525bd8591cd78bd5ee48
Cong Minh <congndm@rabiloo.com>
2025-03-05 17:25:29 +0700

    fix: continue

--- src/main/java/com/example/spring/ApplicationController.java
+++ src/main/java/com/example/spring/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping()
 public class ApplicationController {
+
 	@GetMapping()
 	String helloSpringApp() {
 		return "Hello Spring App";
